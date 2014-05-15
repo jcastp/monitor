899ca838f9a9b73e9c9a6bcb66b47e093f582f5d
Javier Castilla <jcastp@gmail.com>
2014-05-15 18:58:54 +0200

    Initial Commit

---
+++ monitor.py
@@ -0,0 +1,22 @@
+import psutil
+
+# Read the options from file
+## Different files for diferent systems: cpu, mem, disks, etc.
+
+
+# Check the different systems:
+
+# Check CPU
+## Check Thresholds
+
+
+# Check MEM
+## Check Thresholds
+
+
+# Check Disks
+## Check Thresholds
+
+
+# Check processes
+## Check all the processes are running

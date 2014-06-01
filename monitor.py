202083c630a6ab0fa6bd284008e1275d0dea7b1c
Javier Castilla <jcastp@gmail.com>
2014-06-01 09:17:14 +0200

    Used a main function

--- monitor.py
+++ monitor.py
@@ -7,94 +7,94 @@ from readcfg.readcfg import *
 from monitors import cpu, mem, disks, process
 
 
-## Here will be the command line arguments
-parser = argparse.ArgumentParser()
-parser.add_argument("--config",
+def main():
+
+    ## Here will be the command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config",
                     help="Specify where the config file is",
                     action="store",
                     default = "./config/config.cfg"
                    )
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
 
-#####################################
-# Reading the options
-#####################################
+    #####################################
+    # Reading the options
+    #####################################
 
 
-# Use a general config file, and derive the rest from that
-config_file = args.config
+    # Use a general config file, and derive the rest from that
+    config_file = args.config
 
-# Get the configuration files
-config_files_dict = read_general_config(config_file)
-print config_files_dict
+    # Get the configuration files
+    config_files_dict = read_general_config(config_file)
+    #print config_files_dict
 
-## Read all the options
-# cpu options
-cpu_options = read_particular_options(config_files_dict["cpu_options"])
-# mem options
-mem_options = read_particular_options(config_files_dict["mem_options"])
-# disks options
-disks_options = read_disks_options(config_files_dict["disks_options"])
-# process options
-process_options = read_process_options(config_files_dict["process_options"])
+    ## Read all the options
+    # cpu options
+    cpu_options = read_particular_options(config_files_dict["cpu_options"])
+    # mem options
+    mem_options = read_particular_options(config_files_dict["mem_options"])
+    # disks options
+    disks_options = read_disks_options(config_files_dict["disks_options"])
+    # process options
+    process_options = read_process_options(config_files_dict["process_options"])
 
-# DEBUG
-print cpu_options, mem_options, disks_options, process_options
-
-
-## Main part of the application
-
-##############################
-# Get machine information
-##############################
-# Get OS
-systemtype = sys.platform
-# Get the hostname
-hostname = socket.gethostname()
-
-# TODO Get the IP
-# Get the IP
+    # DEBUG
+    #print cpu_options, mem_options, disks_options, process_options
 
 
+    ## Main part of the application
+
+    ##############################
+    # Get machine information
+    ##############################
+    # Get OS
+    system_type = sys.platform
+    # Get the hostname
+    hostname = socket.gethostname()
+
+    # TODO Get the IP
+    # Get the IP
 
 
-################################
-# Main loop of the application
-################################
+    ################################
+    # Main loop of the application
+    ################################
 
-# Get the heartbeat signal
-# TODO Does it makes sense if we send the data once a minute?
+    # Get the heartbeat signal
+    # TODO Does it makes sense if we send the data once a minute?
 
-# Check the different systems:
+    # Check the different systems:
 
-# Get CPU usage
-cpu_usage = cpu.get_cpu()
-## Check Thresholds
-# TODO
-
-
-# Get MEM
-mem_data = mem.get_mem()
-## Check Thresholds
-# TODO
+    # Get CPU usage
+    cpu_usage = cpu.get_cpu()
+    ## Check Thresholds
+    # TODO
 
 
-# Check Disks
-partitions = disks.get_partitions()
-disks_data = disks.get_disk_usage(partitions)
-## Check Thresholds
+    # Get MEM
+    mem_data = mem.get_mem()
+    ## Check Thresholds
+    # TODO
 
 
-# Check processes
-## Check all the processes are running
+    # Check Disks
+    partitions = disks.get_partitions()
+    disks_data = disks.get_disk_usage(partitions)
+    ## Check Thresholds
+
+
+    # Check processes
+    ## Check all the processes are running
 
 
 
 
 
-print cpu_usage, mem_data, disks_data
+    print cpu_usage, mem_data, disks_data
 
-#if __name__ == "__main__":
-#    main()
+if __name__ == "__main__":
+    main()
--- monitors/disks.py
+++ monitors/disks.py
@@ -11,27 +11,31 @@ disk_info = collections.namedtuple("disk_info", ['device', 'mountpoint',
 
 def get_partitions():
     """We get a named tuple with the data of the physical partitions
-    present on the machine
+    present on the machine.
     """
     partitions = psutil.disk_partitions()
     return partitions
     
 
-def get_disk_usage(partitions):
+def get_disk_usage(partitions, monitored=None):
     """Given the partitions in the machine, we select only the ones present
     in the config file to be monitorized, and we check the usage data
     of those.
+    Arguments:
+        - partitions: a named tuple of the partitions present on the machine
+        - monitored: a list of partitions we want to monitor.
+    Returns: a list of named tuples
     """
     disks_data = []
     # TODO Select only those that are monitorized
     for disk in partitions:
+        d_usage = psutil.disk_usage(disk.mountpoint)
         # Here, we join the two named tuples, so we can
         # get all the information
-        d_usage = psutil.disk_usage(disk.mountpoint)
         d_info = disk_info(disk.device, disk.mountpoint, disk.fstype,
                             disk.opts, d_usage.total, d_usage.used,
                             d_usage.free, d_usage.percent)
-        #disk_info = disk + psutil.disk_usage(disk.mountpoint) 
+        # Add the data to the list
         disks_data.append(d_info)
     return disks_data
 
